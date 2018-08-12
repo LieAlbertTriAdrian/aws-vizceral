60e7fd1c6e0e7363ba5df2de099008c59f3cdc28
Albert Lie <alberttri23@gmail.com>
2018-08-12 17:25:53 +0700

    add aws instance mapping

--- scripts/vpc_flow_parser.js
+++ scripts/vpc_flow_parser.js
@@ -1,13 +1,19 @@
-var _ = require('underscore');
-var fs = require('fs');
-var parse = require('csv-parse');
+'use strict'
 
-var data = fs.readFileSync('./sample_vpc_flow.log','utf8')
+const _ = require('underscore');
+const fs = require('fs');
+const parse = require('csv-parse');
+const AWS = require('aws-sdk');
+const async = require('async');
+const awsEC2 = new AWS.EC2({
+    region: 'us-west-2'
+});
+const data = fs.readFileSync('./sample_vpc_flow.log','utf8')
 
-function constructVizceralNodesJSONData (nodes) {
+function constructVizceralNodesJSONData (nodes, ipDictionary) {
 	var jsonData = _.map(nodes, function (node) {
 		return {
-			name: node,
+			name: ipDictionary[node] ? ipDictionary[node] : node,
 			renderer: 'focusedChild',
 			class: 'normal'
 		};
@@ -16,14 +22,14 @@ function constructVizceralNodesJSONData (nodes) {
 	return jsonData;
 }
 
-function constructVizceralConnectionsJSONData (connections) {
+function constructVizceralConnectionsJSONData (connections, ipDictionary) {
 	var jsonData = _.map(connections, function (connection) {
-		var source = connection.split('-')[0];
-		var target = connection.split('-')[1];
+		var source = connection.split(';')[0];
+		var target = connection.split(';')[1];
 
 		return {
-			source: source,
-			target: target,
+			source: (ipDictionary[source]) ? ipDictionary[source] : source,
+			target: ipDictionary[target] ? ipDictionary[target] : target,
 			metrics: {
 				danger: 116.524,
 				normal: 15598
@@ -34,7 +40,6 @@ function constructVizceralConnectionsJSONData (connections) {
 
 	return jsonData;
 }
-
 
 function constructVizceralFinalJSONData (nodes, connections) {
 	return {
@@ -71,6 +76,36 @@ function constructVizceralFinalJSONData (nodes, connections) {
 	}
 }
 
+function mapIpsToAWSInstanceName (nodes, callback) {
+	const params = {
+		Filters: [
+			{
+				Name: 'private-ip-address',
+				Values: nodes
+			}
+		]
+	};
+
+    awsEC2.describeInstances(params , function (err, result) {
+    	if (err) {
+    		console.log(err);
+    		process.exit(-1);
+    	}
+
+    	const instances = result.Reservations;
+
+    	async.map(instances, function (instance, next) {
+    		var mappedData = {
+    			instance_id: instance.Instances[0].InstanceId,
+    			name: _.findWhere(instance.Instances[0].Tags, { Key: 'Name' }).Value,
+    			private_ip: instance.Instances[0].PrivateIpAddress
+    		};
+
+    		next(null, mappedData);
+    	}, callback)
+    });
+}
+
 parse(data, { delimiter: ' ' }, function (err, output) {
 	if (err) {
 		console.log(err);
@@ -88,7 +123,7 @@ parse(data, { delimiter: ' ' }, function (err, output) {
 
 		allNodes.push(datum[3]);
 		allNodes.push(datum[4]);
-		allConnections.push(`${datum[3]}-${datum[4]}`);
+		allConnections.push(`${datum[3]};${datum[4]}`);
 
 		var jsonData = {
 			source_address: datum[3],
@@ -101,12 +136,25 @@ parse(data, { delimiter: ' ' }, function (err, output) {
 	var allUniqueNodes = _.uniq(allNodes);
 	var allUniqueConnections = _.uniq(allConnections);
 
-	var vizceralNodes = constructVizceralNodesJSONData(allUniqueNodes);
-	var vizceralConnections = constructVizceralConnectionsJSONData(allUniqueConnections);	
-	var finalJSONData = constructVizceralFinalJSONData(vizceralNodes, vizceralConnections)
+	mapIpsToAWSInstanceName(allUniqueNodes, function (err, data) {
+		if (err) {
+			console.log(err);
+			process.exit(-1);
+		}
 
-	fs.writeFileSync('result.json', JSON.stringify(finalJSONData, null, 4));
+		var ipDictionary = {};
 
-	process.exit(0);
+		_.each(data, function (datum) {
+			ipDictionary[datum.private_ip] = datum.name
+		});
+
+		var vizceralNodes = constructVizceralNodesJSONData(allUniqueNodes, ipDictionary);
+		var vizceralConnections = constructVizceralConnectionsJSONData(allUniqueConnections, ipDictionary);	
+		var finalJSONData = constructVizceralFinalJSONData(vizceralNodes, vizceralConnections)
+
+		fs.writeFileSync('result.json', JSON.stringify(finalJSONData, null, 4));
+
+		process.exit(0);
+	});
 });
 
